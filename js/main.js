1599dc55910a5ef273dce1c4d79cd1ed440b7614
Malash Dzmitrij <d.malash@flex.by>
2018-02-23 14:48:41 +0300

    adaprive, mob-menu, agree-checkbox

--- js/main.js
+++ js/main.js
@@ -1,3 +1,35 @@
+//new funcrtion
+
+$('.js-mob-menu').off('click touchstart').on('click ', function(e){
+    $('.mob-menu').toggleClass('active');
+    $('html').toggleClass('menu-active');
+})
+
+var mob_menu = $('.mob-menu .wrap-list');
+mob_menu.each(function(){
+    var _t = $(this);
+    _t.off('click ').on('click', function(e){
+        var second = _t.find('.second-level'),
+            second_item = second.find('li');
+        if(_t.hasClass('visible')){
+            if(second.is(e.target) || second_item.is(e.target)){
+                console.log('qq')
+                return false;
+            } else {
+                console.log('dd')
+                _t.removeClass('visible');
+                return false;
+            }
+            _t.removeClass('visible');
+            return false;
+        } else {
+            _t.addClass('visible').siblings().removeClass('visible');
+            return false;
+        }
+    });
+})
+// $('.mob-menu .wrap-list')
+//new funcrtion
 
 
 // helpers func for class 
